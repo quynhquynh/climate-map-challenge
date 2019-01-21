81c6ace4d1d593947f6f7aa656aa4ea20d93132b
Quynh Le <q.lenhuquynh@gmail.com>
2019-01-22 01:05:13 +0200

    Updates

---
+++ precache-manifest.7dbaf6b501239758396351c51041a481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "35ea640fec686c13d995",
+    "url": "/climate-map-challenge/static/js/runtime~main.35ea640f.js"
+  },
+  {
+    "revision": "4f6c2436a3697fc5b7f5",
+    "url": "/climate-map-challenge/static/js/main.4f6c2436.chunk.js"
+  },
+  {
+    "revision": "e5920242957152b0f6e7",
+    "url": "/climate-map-challenge/static/js/1.e5920242.chunk.js"
+  },
+  {
+    "revision": "4f6c2436a3697fc5b7f5",
+    "url": "/climate-map-challenge/static/css/main.0175fa21.chunk.css"
+  },
+  {
+    "revision": "e5920242957152b0f6e7",
+    "url": "/climate-map-challenge/static/css/1.19fbb728.chunk.css"
+  },
+  {
+    "revision": "0f8226f1d937168a5bf13f75797c00c5",
+    "url": "/climate-map-challenge/index.html"
+  }
+];
